58f9fb440f5bcafff8e5855d1d410b3181e85ad1
Conso Kabondo <conso@example.com>
2026-06-16 18:24:06 +0100

    Use SSL for Render external database URL

--- AppMwinda/settings.py
+++ AppMwinda/settings.py
@@ -119,11 +119,12 @@ DATABASES = {
 }
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
+
 if DATABASE_URL:
-    DATABASES['default'] = dj_database_url.config(
-        default=DATABASE_URL,
+    DATABASES['default'] = dj_database_url.parse(
+        DATABASE_URL,
         conn_max_age=600,
-        ssl_require=False
+        ssl_require=True
     )
 
 # ========================
